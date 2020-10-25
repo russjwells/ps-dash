a9ffa49dfb40d5787d33424161d152a2d3767377
Russ Wells <russ@russdigital.com>
2020-10-24 23:11:54 -0700

    added a test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,12 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
 });
+
+test('data is loading and routes work on / path load', () => {
+  const { getByText } = render(<App />)
+  expect(getByText("Fourth of July Promo")).not.toBeNull()
+})
