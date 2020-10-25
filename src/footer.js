addea691c5b324c28b87de233f03d41ce261cc17
Russ Wells <russ@russdigital.com>
2020-10-25 15:46:06 -0700

    spacing in footer

--- src/footer.js
+++ src/footer.js
@@ -6,12 +6,12 @@ import LaunchPad from './launchPad.js'
 const Footer = () => {
     const Data = useContext(DashContext)
     return (
-        <div style={{backgroundColor: 'black', color: 'white', fontWeight: 'bold', padding:'10px', flexDirection: "row", display: "flex", justifyContent: 'space-between'}}>
-            <div >
+        <div style={{backgroundColor: 'black', color: 'white', fontWeight: 'bold', padding:'10px', flexDirection: "row", display: "flex", justifyContent: 'flex-start'}}>
+            <div style={{marginRight: '75px', marginLeft: '25px'}}>
             🚀 Launch Pad 
             </div>
             <LaunchPad />
-            <div style={{textAlign:"end"}}>
+            <div style={{marginLeft:'auto'}}>
             Happy Sending ✉️💨
             </div>
         </div>
